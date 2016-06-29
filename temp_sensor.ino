6fcf08de303cb3238cb44c3cded8a2d99f28d2b2
shubhamchopra3 <shubhamchopra553@gmail.com>
2016-06-29 14:51:01 +0530

    Add files via upload

---
+++ temp_sensor.ino
@@ -0,0 +1,53 @@
+#include <dht.h>
+
+dht DHT;
+
+#define DHT11_PIN 5
+
+void setup()
+{
+  Serial.begin(9600);
+  
+  Serial.println();
+  Serial.println("Type,\tstatus,\tHumidity (%),\tTemperature (C)");
+}
+
+void loop()
+{
+  // READ DATA
+  Serial.print("DHT11, \t");
+  int chk = DHT.read11(DHT11_PIN);
+  switch (chk)
+  {
+    case DHTLIB_OK:  
+                Serial.print("OK,\t"); 
+                break;
+    case DHTLIB_ERROR_CHECKSUM: 
+                Serial.print("Checksum error,\t"); 
+                break;
+    case DHTLIB_ERROR_TIMEOUT: 
+                Serial.print("Time out error,\t"); 
+                break;
+    case DHTLIB_ERROR_CONNECT:
+        Serial.print("Connect error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_L:
+        Serial.print("Ack Low error,\t");
+        break;
+    case DHTLIB_ERROR_ACK_H:
+        Serial.print("Ack High error,\t");
+        break;
+    default: 
+                Serial.print("Unknown error,\t"); 
+                break;
+  }
+  // DISPLAY DATA
+  Serial.print(DHT.humidity);
+  Serial.print(",\t");
+  Serial.print(",\t");
+  Serial.println(DHT.temperature);
+
+  delay(2000);
+}
+//
+// END OF FILE
